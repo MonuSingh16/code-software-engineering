46e964668e4ba3296ef88c6163cbda9307c63be5
Monu Singh <monu.singh9203@gmail.com>
2023-06-23 12:17:50 +0530

    Add tutorial for OOPs

--- 02-OOPs-Concept/main.py
+++ 02-OOPs-Concept/main.py
@@ -1,65 +1,20 @@
-import csv
+from item import Item
 
-class Item:
-    
-    pay_rate = 0.8 # Class attribute. The pay rate after 20% dicount
-    all = []
+item1 = Item("MyItem", 750)
+item1.name = "OtherItem"
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        # Run validation to receive arguments
-        assert price >= 0, f"Price {price} is not greater than or equal to zero"
-        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero"
-        
-        # Assign to self object
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        #print(f'An instance created: {name}')
+print(item1.name)
 
-        # Actions to execute
-        Item.all.append(self)
 
-    def calculate_total_price(self):
-         # This a method, The first parameter is object itself
-         # We have now access to attributes from self 
-         return self.price * self.quantity
-    
-    def apply_discount(self):
-        # Updating the price attribute with old price and multiplied with pay rate
-        # Access the pay rate from the class level, if not then use instance level
-        self.price = self.price * self.pay_rate
-    
-    @classmethod
-    def instantiate_from_csv(cls):
-        # This method should no be called from instance
-        # Here the class reference is passed as parameter
-        with open('items.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            items = list(reader)
-        
-        for item in items:
-            Item(
-                name = item.get('name'),
-                price = float(item.get('price')),
-                quantity = int(item.get('quantity'))
-            )
 
-    @staticmethod
-    def is_integer(num):
-        # We will count out the floats that are point zero
+#phone1 = Phone("jscPhonev10", 500, 5, 1)
+#print(Phone.all)
+#print(Item.all)
 
-        if isinstance(num, float):
-            # Count out the floats that are point zero
-            return num.is_integer()
-        elif isinstance(num, int):
-            return True
-        else:
-            return False
 
-    def __repr__(self) -> str:
-        return f"Item('{self.name}', '{self.price}', '{self.quantity}')"
+#phone2 = Phone("jscPhonev11", 700, 10, 1)
 
-print(Item.is_integer(7.0))
+#print(Item.is_integer(7.0))
 
 #Item.instantiate_from_csv() # Instantiate a class method
 #print(Item.all)
