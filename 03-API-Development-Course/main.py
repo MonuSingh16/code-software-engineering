cd3b2565a5aff60c8ebf0308b67a9a00386e0ef6
Monu Singh <monu.singh9203@gmail.com>
2023-06-28 18:08:51 +0530

    Working on the course

---
+++ 03-API-Development-Course/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# "@"" -> A Decorator. "get" -> HTTP Method for retrieving data as request. "/"" -> Path Operation
+# In case of same path operation, then the first match path outputs. Hence, Order matters.
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my API"}
+
+@app.get("/posts")
+def get_posts():
+    return  {"data": "This is your post"}
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
+
+@app.post("/createnewposts")
+def create_posts(new_post: Post):
+    print(new_post.title)
+    return {"data": "new post"}
+
+# title -> str, content -> str
